c1eea93d85575d68c8f2779934678d43899adaf6
kamedon <hkamei@gmail.com>
2020-11-18 14:19:29 +0900

    add p-022

--- problem/src/bin/p-021.rs
+++ problem/src/bin/p-021.rs
@@ -5,7 +5,7 @@ use regex::{Regex};
 fn main() {
     let reader_file_gz = reader("./assets/jawiki-country.json.gz");
     let re = Regex::new(r"^\[\[Category:.*\]\]$").unwrap();
-    for json in reader_file_gz.lines().take(10) {
+    for json in reader_file_gz.lines() {
         let json = json.unwrap();
         for line in json.split("\\n").into_iter() {
             if re.is_match(line) {
--- problem/src/bin/p-022.rs
+++ problem/src/bin/p-022.rs
@@ -4,8 +4,8 @@ use regex::{Regex};
 
 fn main() {
     let reader_file_gz = reader("./assets/jawiki-country.json.gz");
-    let re = Regex::new(r"\[\[Category:(.*)\]\]$").unwrap();
-    for json in reader_file_gz.lines().take(10) {
+    let re = Regex::new(r"^\[\[Category:([^|]*).*\]\]$").unwrap();
+    for json in reader_file_gz.lines() {
         let json = json.unwrap();
         for line in json.split("\\n").into_iter() {
             for cap in re.captures_iter(line) {
