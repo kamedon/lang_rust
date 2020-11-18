ef0a91e55fcffa1452a0487d012687fb0a474606
kamedon <hkamei@gmail.com>
2020-11-18 15:29:32 +0900

    add p-024

---
+++ problem/src/bin/p-024.rs
@@ -0,0 +1,18 @@
+use lang_lib::file::reader;
+use std::io::BufRead;
+use regex::{Regex, Captures, Error};
+use std::fs::read_to_string;
+
+fn main() {
+    let reader_file_gz = reader("./assets/jawiki-country.json.gz");
+    let re = Regex::new(r"File:(.*?)\|").unwrap();
+    for json in reader_file_gz.lines() {
+        let json = json.unwrap();
+        for line in json.split("\\n").into_iter() {
+            for cap in re.captures_iter(line) {
+                println!("{}", &cap[1]);
+            }
+        }
+    }
+}
+
