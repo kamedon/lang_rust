4fc6ef4e2a9d7964aa049c60befb68153f4420e9
kamedon <hkamei@gmail.com>
2020-11-19 16:02:26 +0900

    add p-026

--- problem/src/bin/p-026.rs
+++ problem/src/bin/p-026.rs
@@ -7,11 +7,13 @@ fn main() {
     let reader_file_gz = reader("./assets/jawiki-country.json.gz");
     let basic_info_re = Regex::new(r"\{\{基礎情報(.*)\}\}").unwrap();
     let field_re = Regex::new(r"\|(.+?)=(.+?)\\n").unwrap();
-    for json in reader_file_gz.lines().take(3) {
+    let remove_string_re = Regex::new(r"'{2,5}").unwrap();
+    for json in reader_file_gz.lines().take(1) {
         let json = json.unwrap();
-        for cap in basic_info_re.captures_iter(json.as_str()) {
+        let str = remove_string_re.replace_all(json.as_str(), "");
+        for cap in basic_info_re.captures_iter(str.as_ref()) {
             for cap2 in field_re.captures_iter(&cap[1]) {
-                println!("{} = {}", &cap2[1].trim(), &cap2[2].trim());
+                println!("{} = {}", &cap2[1].trim(), &cap2[2]);
             }
         }
     }
