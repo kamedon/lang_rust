e87114e3878749612f065847507cd271e0459c50
kamedon <hkamei@gmail.com>
2020-12-04 09:01:26 +0900

    リファクタリング35

--- problem/src/bin/p-035.rs
+++ problem/src/bin/p-035.rs
@@ -2,19 +2,16 @@ use lang_lib::mecab::read_file;
 use std::collections::HashMap;
 
 fn main() {
-    let mut hash_map: HashMap<String, u32> = HashMap::new();
-    read_file("./assets/neko.txt.mecab").into_iter()
-        .for_each(|sentence| {
-            sentence.morphemes.iter().for_each(|morpheme| {
-                let key = morpheme.surface.clone();
-                let current = match hash_map.get(&*key) {
-                    None => { 1 }
-                    Some(v) => { *v + 1 }
-                };
-                hash_map.insert(key, current);
-            })
-        });
+    let mut hash_map: HashMap<&str, u32> = HashMap::new();
+    let sentences = read_file("./assets/neko.txt.mecab");
+    sentences.iter().for_each(|sentence| {
+        sentence.morphemes.iter().for_each(|morpheme| {
+            let key = &morpheme.surface;
+            *hash_map.entry(key).or_default() += 1;
+        })
+    });
 
-    let mut vec: Vec<(&String, &u32)> = hash_map.iter().collect();
+    let mut vec: Vec<_> = hash_map.iter().collect();
     vec.sort_by(|a, b| b.1.cmp(a.1));
     println!("{:?}", vec);
+}
