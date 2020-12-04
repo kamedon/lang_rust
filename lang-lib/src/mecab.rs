06a848831f2ac9ff2dd09cdd69ff598e581dd329
kamedon <hkamei@gmail.com>
2020-12-04 09:42:34 +0900

    リファクタリング

--- lang-lib/src/mecab.rs
+++ lang-lib/src/mecab.rs
@@ -3,46 +3,52 @@ use std::io::{self, BufRead, BufReader};
 use std::collections::HashMap;
 
 #[derive(Debug)]
-pub struct Morpheme {
+pub struct Morpheme<'a> {
     //表層系
-    pub surface: String,
+    pub surface: &'a str,
     //基本形
-    pub base: String,
+    pub base: &'a str,
     //品詞
-    pub pos: String,
+    pub pos: &'a str,
     //品詞細分類1
-    pub pos1: String,
+    pub pos1: &'a str,
 }
 
 #[derive(Debug)]
-pub struct Sentence {
-    pub morphemes: Vec<Morpheme>
+pub struct Sentence<'a> {
+    pub morphemes: Vec<Morpheme<'a>>
 }
 
-pub fn convert_morpheme(str: &str) -> Morpheme {
-    let mut split_line = str.split("\t");
+pub fn to_morpheme(line: &str) -> Morpheme {
+    let mut split_line = line.split("\t");
     let surface = split_line.next().unwrap();
-    let other: Vec<_> = split_line.next().unwrap().split(",").collect();
+    let mut other = split_line.next().unwrap().split(",");
+    let pos = other.next().unwrap();
+    let pos1 = other.next().unwrap();
+    let base = other.skip(4).next().unwrap();
+
+
     Morpheme {
-        surface: String::from(surface),
-        pos: String::from(other[0]),
-        pos1: String::from(other[1]),
-        base: String::from(other[6]),
+        surface,
+        pos,
+        pos1,
+        base,
     }
 }
 
 pub fn read_file(path: &str) -> Vec<Sentence> {
+    // let file = BufReader::new(File::open(path).unwrap());
+    let file = include_str!("../../problem/assets/neko.txt.mecab");
     let mut sentences: Vec<Sentence> = vec![Sentence { morphemes: vec![] }];
-
-    for line in BufReader::new(File::open(path).unwrap()).lines() {
-        let line = line.unwrap();
+    let lines = file.lines();
+    for line in lines {
         if line == "EOS" {
             sentences.push(Sentence {
                 morphemes: vec![]
             });
             continue;
         }
-        let morpheme = convert_morpheme(&line);
+        let morpheme = to_morpheme(&line);
         sentences.last_mut().unwrap().morphemes.push(morpheme)
     }
     sentences
