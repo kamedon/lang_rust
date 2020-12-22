649d8edf386dc204a856156b6b9adcd13ba46b65
kamedon <hkamei@gmail.com>
2020-12-22 10:05:01 +0900

    add 044

---
+++ problem/src/bin/p-044.rs
@@ -0,0 +1,37 @@
+use lang_lib::cabocha::{convert_sentences, Morph, Chunk};
+use itertools::Itertools;
+use std::collections::HashMap;
+use std::fs::{File, OpenOptions};
+use std::io::{self, Read, Write, BufReader, BufWriter};
+
+fn f<'a>(c: &'a Chunk) -> Vec<&'a str> {
+    c.morphs.iter().filter(|m| m.pos == "助詞").map(|m| m.surface).collect::<Vec<_>>()
+}
+
+fn main() {
+    let sentences = convert_sentences(include_str!("../../assets/ai.ja.txt.parsed"));
+    let file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open("output/044.txt").unwrap();
+    let mut writer = BufWriter::new(file);
+    for sentence in sentences {
+        let chunks = sentence.chunks;
+        for chunk in chunks.iter() {
+            let m = chunk.morphs.iter().find(|m| m.pos == "動詞");
+            match m {
+                None => {}
+                Some(m) => {
+                    let words = chunks.iter()
+                        .filter(|&c| c.srcs == chunk.dst as i32)
+                        .map(|c| f(c))
+                        .flatten().join(" ");
+
+                    let line = format!("{}\t{}\n", m.base, words);
+
+                    writer.write(line.as_bytes()).unwrap();
+                }
+            }
+        }
+    }
+}
